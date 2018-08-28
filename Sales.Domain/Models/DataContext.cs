b4d15ca50bcd23eaa43a33b4756f8ec5872e55c6
AlexCasdu <42116081+AlexCasdu@users.noreply.github.com>
2018-08-27 22:38:44 -0500

    Class 25

--- Sales.Domain/Models/DataContext.cs
+++ Sales.Domain/Models/DataContext.cs
@@ -1,13 +1,13 @@
 ﻿namespace Sales.Domain.Models
 {
     using System.Data.Entity;
+    using Common.Models;
 
     public class DataContext : DbContext
     {
         public DataContext() : base("DefaultConnection")
         {
         }
-
-        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
